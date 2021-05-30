6ee63bc22be7755074042e362172228be5be8bc4
voidsentinel <mgpatry@gmail.com>
2021-05-30 22:23:03 +0200

    small opt

--- program.cs
+++ program.cs
@@ -13,10 +13,10 @@ namespace Hforce
             // get the rooms & doors template
             MapTemplateList doors = MapTemplateLoader.loadFromDirectory("./assets/doors", "doors");
             // get the cleaning template rules
-            ReplacementRuleList modifications = ReplacementRuleLoader.loadFromDirectory("./assets/modifications", "modifciations");
+            ReplacementRuleList modifications = ReplacementRuleLoader.loadFromDirectory("./assets/modifications", "modifications");
 
             // create the map
-            Map map = new Map(50, 50);
+            Map map = new Map(100, 100);
             // create the generator & call it
             MapGenerator1 generator = new MapGenerator1(rooms, doors, modifications);
             generator.GenerateMap(map);
